9493d944571aeabd0b1a6c0fbcb56114e6352fcd
Amir Yunas <ayunas@gmail.com>
2019-12-26 22:26:08 -0600

    implemented remove() __remove_node__() and find() methods in the doubly linked list, appears to be functioning correctly

--- queue_and_stack/dll.py
+++ queue_and_stack/dll.py
@@ -37,7 +37,31 @@ class DLinkedList:
         else:
             self.tail.next = Node(val,self.tail,None)
             self.tail = self.tail.next
+
+    def __remove_node__(self,node):
+        if node is self.head:
+            # node.next = None
+            self.head = self.head.next
+        if node.prev:
+            node.prev.next = node.next
+        if node.next:
+            node.next.prev = node.prev
+        node.prev = None
+        node.next = None
     
+    def remove(self,val):
+        found  = self.find(val)
+        if found:
+            self.__remove_node__(found)
+        else:
+            return
+    
+    def find(self,val):
+        n = self.head
+        while n and n.value != val:
+            n = n.next
+        return n
+
 dll = DLinkedList()
 dll.append(30)
 dll.append(35)
@@ -45,8 +69,12 @@ dll.append(40)
 dll.append(50)
 dll.prepend(10)
 dll.prepend(5)
-# print(dll.head.next.next.prev)
 print(dll)
+dll.remove(30)
+print(dll)
+
+
+
 
 
 
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -2,17 +2,15 @@
 # sys.path.append('../doubly_linked_list')
 # from doubly_linked_list import DoublyLinkedList
 
-from dll import DoublyLinkedList
-
+from dll import DLinkedList
 
 class Stack:
     def __init__(self):
         self.size = 0
-        # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DLinkedList()
 
     def push(self, value):
-        pass
+        self.storage.prepend(value)
 
     def pop(self):
         pass
